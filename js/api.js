65d4fda223ab5159534f2ffb4aed25aaef3f9c77
freenet-365 <AnonKeySolutions@gmail.com>
2024-06-19 00:51:13 +0200

    Add files via upload

--- js/api.js
+++ js/api.js
@@ -2,7 +2,7 @@ function download(filename) {
     // Create a temporary anchor element
     var link = document.createElement('a');
     link.href = './configs/' + filename;
-     link.download=filename;
+    link.download = filename;
     // Append the link to the body
     document.body.appendChild(link);
 
@@ -11,5 +11,19 @@ function download(filename) {
 
     // Remove the link from the document
     document.body.removeChild(link);
-     fileItem.style.border = "14px solid #25d366"; // Rounded corners
+
+    // AJAX request to send the filename to the server
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://freenet-365.000webhostapp.com/logs.php", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            console.log("Filename logged successfully");
+        }
+    };
+    xhr.send("filename=" + encodeURIComponent(filename));
+
+    if (onFire) {
+        onFire.style.border = "14px solid #25d366"; // Rounded corners
+    }
 }
